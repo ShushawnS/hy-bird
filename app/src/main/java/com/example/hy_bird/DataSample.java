b696e9778ae0b0c64eb47611619bb90670b0c6cc
Shushawn Saha <shushawn.saha@gmail.com>
2022-02-06 05:41:56 -0500

    created algo for date scores

---
+++ app/src/main/java/com/example/hy_bird/DataSample.java
@@ -0,0 +1,95 @@
+package com.example.hy_bird;
+
+public class DataSample {
+    String name, monday, tuesday, wednesday, thursday, friday;
+    int capacity;
+    String bMeeting, tMeeting, transportation, para;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMonday() {
+        return monday;
+    }
+
+    public void setMonday(String monday) {
+        this.monday = monday;
+    }
+
+    public String getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(String tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public String getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(String wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public String getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(String thursday) {
+        this.thursday = thursday;
+    }
+
+    public String getFriday() {
+        return friday;
+    }
+
+    public void setFriday(String friday) {
+        this.friday = friday;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getbMeeting() {
+        return bMeeting;
+    }
+
+    public void setbMeeting(String bMeeting) {
+        this.bMeeting = bMeeting;
+    }
+
+    public String gettMeeting() {
+        return tMeeting;
+    }
+
+    public void settMeeting(String tMeeting) {
+        this.tMeeting = tMeeting;
+    }
+
+    public String getTransportation() {
+        return transportation;
+    }
+
+    public void setTransportation(String transportation) {
+        this.transportation = transportation;
+    }
+
+    public String getPara() {
+        return para;
+    }
+
+    public void setPara(String para) {
+        this.para = para;
+    }
+}
--- app/src/main/java/com/example/hy_bird/WeatherApi.java
+++ app/src/main/java/com/example/hy_bird/WeatherApi.java
@@ -146,4 +146,4 @@ public class WeatherApi extends AppCompatActivity{
     }
 
     }
-}
+
